ffa4e572309482736111a135d73875a655a44937
SaberShoker <pronfis29@gmail.com>
2021-02-21 02:36:12 +0400

    Changed idea
    Make 1 main class Hero, 1 interface and 2 class - child of Hero

--- Program.cs
+++ Program.cs
@@ -12,7 +12,16 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            
+            int switchClass;
+
+            Console.WriteLine("You number - ");
+            switchClass = Convert.ToInt32(Console.ReadLine()); 
+
+            Hero man = new Hero("Shoker", 100, 10, switchClass);
+
+            man.Attack();
+
+            Console.WriteLine("It's OK");
         }
     }
 }
